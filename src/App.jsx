702a1376acf46ffdebffb319d11705a2966b4471
Deepak <dk774369@gmail.com>
2023-06-11 12:38:11 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/footer/Footer.';
 import Details from './pages/details/Details';
 import SearchResult from './pages/searchResult/SearchResult';
 import Explore from './pages/explore/Explore';
-import PageNotFound from './pages/404/404';
+import PageNotFound from './pages/404/PageNotFound';
 import { getGenres } from './store/homeSlice';
 
 
